284aa90deff9bc465771c02d086fc643b4643e82
thor-tor <michal.kabara@gmail.com>
2020-11-25 22:45:52 +0100

    this commit adds FaqSection component

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -1,12 +1,14 @@
 import React from "react"
 import AboutSection from "../components/AboutSection"
 import ServicesSection from "../components/ServicesSection"
+import FaqSection from "../components/FaqSection"
 
 const Aboutus = () => {
   return (
     <>
       <AboutSection />
       <ServicesSection />
+      <FaqSection />
     </>
   )
 }

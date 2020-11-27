9da146334b28983f8b11e65908dbf52a467c0ee9
thor-tor <56455625+thor-tor@users.noreply.github.com>
2020-11-28 00:25:53 +0100

    this commit adds Nav component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css"
 import AboutUs from "./pages/AboutUs"
 import GlobalStyle from "./components/GlobalStyle"
+import Nav from "./components/Nav"
 
 const App = () => {
   return (
     <div className="App">
       <GlobalStyle />
+      <Nav />
       <AboutUs />
     </div>
   )
